ba720ce768bb51d952dd019671288285f030f365
DevonGriffith <griffith.devon@gmail.com>
2021-07-10 05:24:13 -0400

    Create Bootsector_Segmentation.asm

---
+++ Bootsector/Bootsector_Segmentation.asm
@@ -0,0 +1,27 @@
+mov ah, 0x0e ; tty
+
+mov al, [the_code]
+int 0x10 ; we know this doesn't work
+
+mov bx, 0x7c0 ; the segment is automatically <<4 for you
+mov ds, bx
+; WARNING: all memory references will be offset by 'ds' implicitly (what segmentation refers to)
+mov al, [the_code]
+int 0x10
+
+mov al, [es:the_code]
+int 0x10 ; This is not right... 'es' currently is 0x000?
+
+mov bx, 0x7c0
+mov es, bx
+mov al, [es:the_code]
+int 0x10
+
+
+jmp $
+
+the_code:
+    db "X"
+
+times 510 - ($-$$) db 0
+dw 0xaa55
