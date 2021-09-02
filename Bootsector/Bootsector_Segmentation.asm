552daa15200e43e7bac12cdaea9c5f6799310582
DevonGriffith <griffith.devon@gmail.com>
2021-09-02 15:41:14 -0400

    Update Bootsector_Segmentation.asm

--- Bootsector/Bootsector_Segmentation.asm
+++ Bootsector/Bootsector_Segmentation.asm
@@ -1,16 +1,16 @@
 mov ah, 0x0e ; tty
 
 mov al, [the_code]
-int 0x10 ; we know this doesn't work
+int 0x10 ; We know this doesn't work already.
 
-mov bx, 0x7c0 ; the segment is automatically <<4 for you
-mov ds, bx
-; WARNING: all memory references will be offset by 'ds' implicitly (what segmentation refers to)
+mov bx, 0x7c0 ; The segment is automatically <<4 for you.
+mov ds, bx ; Segmentation step
+; WARNING: all memory references will be offset by 'ds' implicitly (what segmentation refers to)!
 mov al, [the_code]
 int 0x10
 
 mov al, [es:the_code]
-int 0x10 ; This is not right... 'es' currently is 0x000?
+int 0x10 ; This is not right: 'es' currently is 0x000?
 
 mov bx, 0x7c0
 mov es, bx
@@ -18,9 +18,9 @@ mov al, [es:the_code]
 int 0x10
 
 
-jmp $
+jmp $ ; Jump to $.
 
-the_code:
+the_code: ; Defining "the_code" as "X".
     db "X"
 
 times 510 - ($-$$) db 0
