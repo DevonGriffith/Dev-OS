95125330fde5ca94f78c55675990fa6852b8bdff
DevonGriffith <griffith.devon@gmail.com>
2021-09-02 15:49:16 -0400

    Update Bootsector_Stack.asm

--- Bootsector/Bootsector_Stack.asm
+++ Bootsector/Bootsector_Stack.asm
@@ -1,38 +1,37 @@
 mov ah, 0x0e ; tty mode
 
-mov bp, 0x8000 ; this is an address far away from 0x7c00 so that we don't get overwritten
-mov sp, bp ; if the stack is empty then sp points to bp
+mov bp, 0x8000 ; This is an address far away from 0x7c00 so that we don't get overwritten
+mov sp, bp ; If the stack is empty then sp points to bp
 
 push 'A'
 push 'B'
 push 'C'
 
-; to show how the stack grows downwards
+; To show how the stack grows downwards
 mov al, [0x7ffe] ; 0x8000 - 2
-int 0x10
+int 0x10 ; Display the results.
 
-; however, don't try to access [0x8000] now, because it won't work
-; you can only access the stack top so, at this point, only 0x7ffe (look above)
+; However, don't try to access [0x8000] now, because it won't work anymore.
+; You can only access the stack top so, at this point, only 0x7ffe (look above, line 11).
 mov al, [0x8000]
-int 0x10
+int 0x10 ; Display the results.
 
 
-; recover our characters using the standard procedure: 'pop'
-; We can only pop full words so we need an auxiliary register to manipulate
-; the lower byte
+; Recover our characters using the standard procedure: 'pop'.
+; We can only pop full words so we need an auxiliary register to manipulate the lower byte.
 pop bx
 mov al, bl
-int 0x10 ; prints C
-
-pop bx
-mov al, bl
-int 0x10 ; prints B
+int 0x10 ; Prints C.
 
 pop bx
 mov al, bl
-int 0x10 ; prints A
+int 0x10 ; Prints B.
 
-; data that has been pop'd from the stack is garbage now
+pop bx
+mov al, bl
+int 0x10 ; Prints A.
+
+; Data that has been pop'd from the stack is garbage now (Last In, First Out).
 mov al, [0x8000]
 int 0x10
 
