ad5d96a83841419b5155060a28c53b1a9af5e26f
DevonGriffith <griffith.devon@gmail.com>
2021-09-02 15:30:22 -0400

    Update Simple_Bootsector_Lesson.asm

--- Bootsector/Simple_Bootsector_Lesson.asm
+++ Bootsector/Simple_Bootsector_Lesson.asm
@@ -1,8 +1,8 @@
-The bootsector is the first sector of the disk (cylinder 0, head 0, sector 0) and it takes 512 bytes.
+; The bootsector is the first sector of the disk (cylinder 0, head 0, sector 0) and it takes 512 bytes.
 
-To make sure that the "disk is bootable", the BIOS checks that bytes 511 and 512 of the alleged boot sector are bytes 0xAA55.
+; To make sure that the "disk is bootable", the BIOS checks that bytes 511 and 512 of the alleged boot sector are bytes 0xAA55.
 
-Here is the assembly code for a simple bootsector:
+; Here is the assembly code for a simple bootsector:
 
 ; Infinite loop (e9 fd ff) to keep the bootsector open until we want to close it. 
 loop:
@@ -10,5 +10,5 @@ loop:
 
 ; Fill with 510 zeros minus the size of the previous code (to make up the remained of the 512 bytes).
 times 510-($-$$) db 0
-; add the Magic number as always
+; add the Magic number as always!
 dw 0xaa55 
